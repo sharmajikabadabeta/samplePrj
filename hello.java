b4a6b84956962adb14d717cac9080e5cf8e472db
Harshit <harshits908@gmail.com>
2023-06-28 11:13:38 +0530

    Added the hello.java file

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!\n");
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
