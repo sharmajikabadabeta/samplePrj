14baaa65125443f462d1ee7fea61e2cf4ad19dc0
Harshit <harshits908@gmail.com>
2023-06-28 11:18:15 +0530

    Created index.cpp & modified hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello {
     public static void main(String[] args) {
+
+        // This is a comment
+
         System.out.printf("Hello World!\n");
         
         for (int i = 1; i <= 5; i++) {
